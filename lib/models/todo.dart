fe8c1a1479733acd809fad4a5a95a4dbb3d5c0fb
dinhKhoi-noob <khoip1305@gmail.com>
2021-10-25 12:30:05 +0700

    create NodeJS service

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -5,9 +5,13 @@ class Todo {
   String? description;
   DateTime? dueDate;
   Todo({this.id, this.name, this.priority, this.description, this.dueDate});
-  factory Todo.fromJson(Map<String,dynamic>json) {
+  factory Todo.fromJson(Map<String, dynamic> json) {
     return Todo(
-      id:json['id']
+      id: json['id'],
+      name:json['name'],
+      priority: json['priority'],
+      description: json['description'],
+      dueDate: json['dueDate']
     );
   }
 }
